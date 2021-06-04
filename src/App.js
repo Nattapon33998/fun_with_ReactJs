480b2b4f7e9a266a3092c4dff3d7f142a0e3e0b0
Nattapon33998 <Nattapon33998@gmail.com>
2021-06-04 16:25:40 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  render() {
+
+    const var1 = 0;
+    const var2 = "hi";
+    const var3 = false;
+
+    return (
+      <div>
+        <h1>Nattapon {var1}</h1>
+        <h1>Nattapon {var2}</h1>
+        <h1>Nattapon {var3 && "yes"}</h1>
+        {var3 ? <h1>yes</h1> : <h1>No</h1>}
+      </div>
+    )
+  }
 }
-
-export default App;

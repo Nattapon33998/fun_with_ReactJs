0c81438e993ce8551c9eebdc51c3a15d92681e20
Nattapon33998 <Nattapon33998@gmail.com>
2021-06-04 21:35:59 +0700

    useState

--- src/App.js
+++ src/App.js
@@ -1,28 +1,27 @@
-import React, { Component } from 'react'
+import React, {useState} from 'react'
 
-export default class App extends Component {
-  render() {
+export default function App() {
+  
+  const [count, setCount] = useState(0);
 
-    let var1 = 0;
-    const var2 = "hi";
-    const var3 = false;
+  return (
+    <div>
+      <h1>Count : {count}</h1>
 
-    return (
-      <div>
-        <h1>Nattapon {var1}</h1>
-        <h1>Nattapon {var2}</h1>
-        <h1>Nattapon {var3 && "yes"}</h1>
-        {var3 ? <h1>yes</h1> : <h1>No</h1>}
-
-        <button 
-          onClick={() => {
-            var1  = var1 + 1;
-            console.log(var1)
-          }}
-        >
-          Add
-        </button>
-      </div>
-    )
-  }
+      <button onClick={()=>{
+        setCount(count + 1);
+      }}
+      >
+        Add
+      </button>
+      
+      <button onClick={()=>{
+        setCount(0);
+      }}
+      >
+        Reset
+      </button>
+      
+    </div>
+  )
 }

f3e5368851680e8b294699cb5c964d08d2756ed6
Nattapon33998 <Nattapon33998@gmail.com>
2021-06-04 21:11:27 +0700

    test commit

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 export default class App extends Component {
   render() {
 
-    const var1 = 0;
+    let var1 = 0;
     const var2 = "hi";
     const var3 = false;
 
@@ -13,6 +13,15 @@ export default class App extends Component {
         <h1>Nattapon {var2}</h1>
         <h1>Nattapon {var3 && "yes"}</h1>
         {var3 ? <h1>yes</h1> : <h1>No</h1>}
+
+        <button 
+          onClick={() => {
+            var1  = var1 + 1;
+            console.log(var1)
+          }}
+        >
+          Add
+        </button>
       </div>
     )
   }

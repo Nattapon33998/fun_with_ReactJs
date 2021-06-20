0aca8dcfe1266235178cd4fad5e3705b328aafdb
Nattapon33998 <Nattapon33998@gmail.com>
2021-06-20 16:24:18 +0700

    link & route

--- src/App.js
+++ src/App.js
@@ -5,13 +5,15 @@ import Ex1Style from './components/ex1_styles'
 import Ex2Image from './components/ex2_image'
 import Ex3Props from './components/ex3_props'
 
+import { Link, Route } from 'react-router-dom'
+
 export default function App() {
   
   const [count, setCount] = useState(0);
 
   return (
     <div>
-      <h1>Count : {count}</h1>
+      {/* <h1>Count : {count}</h1>
 
       <button onClick={()=>{
         setCount(count + 1);
@@ -35,7 +37,25 @@ export default function App() {
       <Ex2Image/>
       <Ex2Image image="/images/launch.png"/>
 
-      <Ex3Props count={count} color="#F03" showLabel={true}/>
+      <Ex3Props count={count} color="#F03" showLabel={true}/> */}
+
+      <nav>
+        <ul>
+          <li>
+            <Link to='/'>Ex1</Link>
+          </li>
+          <li>
+            <Link to='/Ex2'>Ex2</Link>
+          </li>
+          <li>
+            <Link to='/Ex3'>Ex3</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <Route exact path="/"><Ex1Style/></Route>
+      <Route path="/Ex2"><Ex2Image/></Route>
+      <Route path="/Ex3"><Ex3Props count={count} color="#F03" showLabel={true}/></Route>
     </div>
   )
 }

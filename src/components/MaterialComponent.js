c43ed337e012168dd409b30d8dbabbe1e614bffb
Nattapon Sukthaworn <nattapon33998@gmail.com>
2021-08-22 23:23:29 +0700

    Button not complet

--- src/components/MaterialComponent.js
+++ src/components/MaterialComponent.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Link, Switch, Route, useRouteMatch } from "react-router-dom";
 
 import MakeStyle from "./Material/MakeStyle";
+import ButtonComponent from "./Material/ButtonComponent";
 
 const MaterialComponent = () => {
   let { path, url } = useRouteMatch();
@@ -10,10 +11,16 @@ const MaterialComponent = () => {
       <ul>
         <Link to={`${url}/makeStyle`}>MakeStyle</Link>
       </ul>
+      <ul>
+        <Link to={`${url}/button`}>Button</Link>
+      </ul>
 
       <Switch>
         <Route path={`${path}/makeStyle`}>
           <MakeStyle red={true} blue={true} />
+        </Route>
+        <Route path={`${path}/>button`}>
+          <ButtonComponent />
         </Route>
       </Switch>
     </div>
